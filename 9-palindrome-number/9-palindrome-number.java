4311005b2799536e62106e25412c7d5cf0769663
Charlie Childress <97141391+charliechildress@users.noreply.github.com>
2022-08-18 20:03:53 -0400

    Time: 18 ms (29.31%), Space: 46.3 MB (15.49%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = String.valueOf(x);
-        StringBuilder s1= new StringBuilder();
-        s1.append(s);
-        return (s.equals(s1.reverse().toString()));
+        String s=String.valueOf(x);StringBuilder s1=new StringBuilder();s1.append(s);return(s.equals(s1.reverse().toString()));
     }
 }
