de9bace14852334882622a835da412a5831cc39b
Charlie Childress <97141391+charliechildress@users.noreply.github.com>
2022-08-22 14:48:22 -0400

    Time: 1 ms (89.83%), Space: 43.9 MB (33.86%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -3,12 +3,11 @@ class Solution {
         int a = cost[0];
         int b = cost[1];
         int temp = 0;
-        
-        for (int i = 2; i < cost.length; i++){
+        for(int i = 2; i < cost.length; i++) {
             temp = Math.min(a, b) + cost[i];
             a = b;
             b = temp;
         }
-        return Math.min(a, b);
+        return Math.min(a,b);
     }
 }
